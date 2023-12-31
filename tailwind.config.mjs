3929d8fc2d82832077cd2164e87dc3eac8f54c80
hfcRed <hfcred@gmx.net>
2023-12-31 23:45:28 +0100

    Added more tailwind styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,12 @@ export default {
 			colors: {
 				textPrimary: '#FFFFFF',
 				textSecondary: '#AFAFB2',
-				action: '#3DACF2',
+				action: '#4990E2',
 				background: '#1E2227',
 				elements: '#2B3038',
+				elementsBright: '#3B414C',
 				borderColor: '#41454C',
+				borderColorBright: '#6E7480',
 			},
 			fontFamily: {
 				'Sans': ['Sans'],
@@ -22,15 +24,17 @@ export default {
 			fontSize: {
 				desktopHeading: '4.25rem',
 				mobileHeading: '3rem',
-				desktopHeading2: '1.56rem',
+				desktopHeading2: '1.8rem',
 				mobileHeading2: '1.5rem',
 				desktopLarge: '1.25rem',
 				mobileLarge: '1.19rem',
 				normal: '1rem',
 				small: '0.8rem',
+				none: '0rem',
 			},
 			backgroundImage: {
-				'radial-gradient': "radial-gradient(at center bottom, #212F40 0%, #090A0F 100%)",
+				'radial-gradient': "radial-gradient(at center top, #373D47 0%, #181B1F 100%)",
+				'linear-gradient': "linear-gradient(180deg, #313740 0%, #181B1F 100%)",
 			},
 			gridTemplateColumns: {
 				'packages': 'repeat(auto-fit, minmax(260px, 1fr))',
@@ -39,9 +43,10 @@ export default {
 			boxShadow: {
 				'packages': '0 0 20px 5px #0000004a',
 				'packagesHover': '0 0 20px 10px #0000004a',
+				'drawer': '0 0 20px 10px #0000004a',
 			},
 			scale: {
-				'1025': '1.025',
+				'102': '1.02',
 			},
 		},
 	},
