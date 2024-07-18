59401b22fb54f2a5ba8996e9c6409d617456062b
hfcRed <elias.horn@gmx.de>
2024-07-18 17:44:41 +0200

    Remove scrollbar placeholder

--- src/components/common/backdrop/backdrop.js
+++ src/components/common/backdrop/backdrop.js
@@ -1,11 +1,7 @@
 let backdrop;
-let scrollbarColor;
-let scrollbarWidth;
 
 document.addEventListener("astro:page-load", () => {
     backdrop = document.querySelector(".backdrop");
-    scrollbarColor = getComputedStyle(document.body).getPropertyValue("--scrollbar-track");
-    scrollbarWidth = window.innerWidth - document.body.clientWidth;
 
     backdrop.onanimationend = () => {
         if (backdrop.getAttribute("data-state") === "closed") { backdrop.classList.add("hidden"); }
@@ -13,10 +9,7 @@ document.addEventListener("astro:page-load", () => {
 });
 
 export function openBackdrop(zIndex, callback) {
-    scrollbarWidth = window.innerWidth - document.body.clientWidth;
-
     document.body.classList.add("overflow-hidden");
-    document.body.style.paddingRight = scrollbarWidth + "px";
 
     backdrop.setAttribute("data-state", "opened");
     backdrop.classList.remove("hidden");
@@ -32,8 +25,6 @@ export function openBackdrop(zIndex, callback) {
             callback();
         }
     }
-
-    createScrollbarPlaceholder(zIndex);
 }
 
 export function closeBackdrop() {
@@ -52,20 +43,4 @@ export function closeBackdrop() {
 
         backdrop.setAttribute("data-state", "closed");
     });
-}
-
-function createScrollbarPlaceholder(zIndex) {
-    const scrollbarPlaceholder = document.createElement("div");
-
-    scrollbarPlaceholder.classList.add("scrollbar-placeholder");
-    scrollbarPlaceholder.style.width = scrollbarWidth + "px";
-    scrollbarPlaceholder.style.height = "100vh";
-    scrollbarPlaceholder.style.position = "fixed";
-    scrollbarPlaceholder.style.top = "0";
-    scrollbarPlaceholder.style.right = "0";
-    scrollbarPlaceholder.style.zIndex = zIndex - 1;
-    scrollbarPlaceholder.style.backgroundColor = scrollbarColor;
-    scrollbarPlaceholder.style.overflow = "hidden";
-
-    document.body.appendChild(scrollbarPlaceholder);
 }
